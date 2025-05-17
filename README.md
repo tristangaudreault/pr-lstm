17b2f6e38242234c57d006ed218c44b480fd2043
Tristan Gaudreault <tristan@tristangaudreault.com>
2025-05-16 22:28:53 -0400

    Improved plot labeling and implemented sequence length control

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Usage
 Run the "[Neural Networks and the Chomsky Hierarchy](https://github.com/google-deepmind/neural_networks_chomsky_hierarchy/tree/main#)" experiments using:
-```bash
-set PYTHONPATH=external\neural_networks_chomsky_hierarchy
+```powershell
+$env:PYTHONPATH = "external"
 py .\experiments\main.py
 ```
--- experiments/main.py
+++ experiments/main.py
@@ -1,32 +1,34 @@
 import functools
-import sys
 from typing import Any, Callable
 import logging
 import argparse
 import pickle
+from filelock import FileLock
 
 import haiku as hk
-import jax
 import jax.nn as jnn
 import jax.numpy as jnp
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from filelock import Timeout, FileLock
 
 import thesis
 
-sys.path.insert(1, "external")
 from neural_networks_chomsky_hierarchy.experiments import (
     constants,
     training,
     utils,
 )
 from neural_networks_chomsky_hierarchy.experiments import curriculum as curriculum_lib
-from neural_networks_chomsky_hierarchy.models import rnn, tape_rnn
 
 
-ARCHITECTURE_PARAMETERS = ["hidden_size", "memory_cell_size", "memory_size"]
+ARCHITECTURE_PARAMETERS = [
+    "hidden_size",
+    "memory_cell_size",
+    "memory_size",
+    "num_branches",
+    "num_heads",
+]
 
 
 def parse_args():
@@ -42,6 +44,7 @@ def parse_args():
     parser.add_argument("-i", "--input", help="path to input file")
     parser.add_argument("-o", "--output", help="path to output file")
     parser.add_argument("--plot", action="store_true", help="display result plot")
+    parser.add_argument("--label", default="architecture", help="key of label value")
 
     # Task parameters
     parser.add_argument(
@@ -51,7 +54,13 @@ def parse_args():
         help="number of samples in each training batch",
     )
     parser.add_argument(
-        "--sequence-length",
+        "--min-sequence-length",
+        type=int,
+        default=1,
+        help="maximum length of training sequences",
+    )
+    parser.add_argument(
+        "--max-sequence-length",
         type=int,
         default=40,
         help="maximum length of training sequences",
@@ -111,12 +120,7 @@ def make_chorus(
 
     def chorus_model(x: jnp.ndarray, input_length: int = 1) -> jnp.ndarray:
         core = rnn_core(**rnn_kwargs)
-        if issubclass(rnn_core, tape_rnn.TapeRNNCore):
-            initial_state = core.initial_state(
-                x.shape[0], input_length
-            )  # pytype: disable=wrong-arg-count
-        else:
-            initial_state = core.initial_state(x.shape[0])
+        initial_state = core.initial_state(x.shape[0])
 
         batch_size, seq_length, embed_size = x.shape
         if seq_length % input_window != 0:
@@ -130,7 +134,7 @@ def make_chorus(
 
         x = hk.Flatten(preserve_dims=2)(x)
 
-        output = core(x, initial_state)
+        _, output = core(x, initial_state)
 
         if not return_all_outputs:
             output = output[:, -1, :]  # (batch, time, alphabet_dim)
@@ -145,7 +149,7 @@ def make_chorus(
 def main(args) -> None:
     # Create the task.
     curriculum = curriculum_lib.UniformCurriculum(
-        values=list(range(1, args.sequence_length + 1))
+        values=list(range(args.min_sequence_length, args.max_sequence_length + 1))
     )
     task = constants.TASK_BUILDERS[args.task]()
 
@@ -156,7 +160,6 @@ def main(args) -> None:
         for k, v in vars(args).items()
         if k in ARCHITECTURE_PARAMETERS and v is not None
     }
-    print(architecture_parameters)
     model = constants.MODEL_BUILDERS[args.architecture](
         output_size=task.output_size,
         return_all_outputs=True,
@@ -208,7 +211,7 @@ def main(args) -> None:
 
     # Gather results and print final score.
     accuracies = [r["accuracy"] for r in eval_results]
-    score = np.mean(accuracies[args.sequence_length + 1 :])
+    score = np.mean(accuracies[args.max_sequence_length + 1 :])
     print(f"Network score: {score}")
 
     return train_results, eval_results, params
@@ -231,19 +234,38 @@ if __name__ == "__main__":
                 except EOFError:
                     break
         if args.plot:
+            text_args = {}
             for result_args, train_results, eval_results in results:
                 ax.plot(
                     eval_results["length"],
                     eval_results["accuracy"],
-                    label=result_args.architecture,
+                    label=getattr(result_args, args.label),
                 )
+                text_args.update(
+                    {
+                        k: v
+                        for k, v in vars(result_args).items()
+                        if k in ARCHITECTURE_PARAMETERS
+                        and k != args.label
+                        and v is not None
+                    }
+                )
+            fig.subplots_adjust(right=0.7)
+            text_args = "\n".join(f"{k}: {v}" for k, v in text_args.items())
+            fig.text(0.75, 0.5, text_args, fontsize=12, va="center")
     else:
         # Monkey patch
-        constants.MODEL_BUILDERS["chorus"] = functools.partial(
-            make_chorus,
-            rnn_core=thesis.model.ChorusRNN,
-            num_branches=2,
-        )
+        new_models = {
+            "chorus": thesis.models.Chorus,
+            "chorus_rnn": thesis.models.ChorusRNN,
+            "chorus_attn": thesis.models.ChorusAttn,
+            "chorus_transformer": thesis.models.ChorusTransformer,
+        }
+        for k, v in new_models.items():
+            constants.MODEL_BUILDERS[k] = functools.partial(
+                make_chorus,
+                rnn_core=v,
+            )
 
         # Run
         train_results, eval_results, params = main(args)
@@ -261,10 +283,11 @@ if __name__ == "__main__":
                     pickle.dump(save_data, f)
 
         if args.plot:
-            x = [result["length"] for result in eval_results]
-            y = [result["accuracy"] for result in eval_results]
-
-            ax.plot(x, y, marker="o")
+            ax.plot(
+                eval_results["length"],
+                eval_results["accuracy"],
+                label=getattr(args, args.label),
+            )
 
     if args.plot:
         ax.set_xlabel("Sequence Length")
