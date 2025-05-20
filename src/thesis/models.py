0fce6c80d9a9f24dc49fcc68ae98bd707e003d76
Tristan Gaudreault <tristan@tristangaudreault.com>
2025-05-20 15:24:47 -0400

    Model source cleanup

--- src/thesis/models.py
+++ src/thesis/models.py
@@ -12,10 +12,9 @@ logger = logging.getLogger(__name__)
 class Chorus(hk.RNNCore):
     def __init__(self, hidden_size, num_branches, name=None):
         super().__init__(name=name)
-
         self.hidden_size = hidden_size
         self.num_branches = num_branches
-
+        
         self.rnn_cell = hk.GRU(hidden_size=hidden_size)
 
     def initial_state(self, batch_size):
@@ -27,24 +26,19 @@ class Chorus(hk.RNNCore):
     def adapt_input(x, num_branches):
         """Adapt the input for use by the Chorus model. Returns an array of shape (batch size, number of branches, branch Length, embedding dimension)"""
         batch_size, seq_len, embed_dim = x.shape
-
         branch_len = -(-seq_len // num_branches)
 
         required_len = num_branches * branch_len
         pad_len = required_len - seq_len
-
         x = jnp.pad(x, ((0, 0), (0, pad_len), (0, 0))) if pad_len > 0 else x
 
         x = x.reshape(batch_size, num_branches, branch_len, embed_dim)
-        x = x.transpose(2, 0, 1, 3).reshape(
+        return x.transpose(2, 0, 1, 3).reshape(
             branch_len, batch_size * num_branches, embed_dim
         )
 
-        return x
-
     def process(self, x, h0):
-        outputs, hx = hk.dynamic_unroll(self.rnn_cell, x, h0)
-        return hx
+        return hk.dynamic_unroll(self.rnn_cell, x, h0)[1]
 
     def __call__(self, x, h0):
         return self.process(self.adapt_input(x, self.num_branches), h0)
@@ -53,16 +47,12 @@ class Chorus(hk.RNNCore):
 class ChorusRNN(Chorus):
     def __init__(self, hidden_size, num_branches, name=None):
         super().__init__(hidden_size, num_branches, name=name)
-
         self.composer = hk.GRU(hidden_size=hidden_size)
 
     def process(self, x, h0):
         branch_hxs = super().process(x, h0[0])
         branch_hxs = jnp.reshape(branch_hxs, (-1, self.num_branches, self.hidden_size))
-        outputs, hx = hk.dynamic_unroll(
-            self.composer, branch_hxs, h0[1], time_major=False
-        )
-        return hx
+        return hk.dynamic_unroll(self.composer, branch_hxs, h0[1], time_major=False)[1]
 
     def initial_state(self, batch_size):
         return super().initial_state(batch_size), self.composer.initial_state(
@@ -71,9 +61,8 @@ class ChorusRNN(Chorus):
 
 
 class ChorusAttn(Chorus):
-    def __init__(self, hidden_size, num_branches, num_heads, name=None):
+    def __init__(self, hidden_size, num_branches, num_heads=4, name=None):
         super().__init__(hidden_size, num_branches, name=name)
-
         self.multihead_attn = hk.MultiHeadAttention(
             num_heads=num_heads,
             key_size=hidden_size // num_heads,
@@ -90,13 +79,12 @@ class ChorusAttn(Chorus):
 
             return hx, output
 
-        return hk.scan(f, h0, x)
+        return hk.scan(f, h0, x)[0]
 
 
 class ChorusTransformer(Chorus):
     def __init__(self, hidden_size, num_branches, name=None):
         super().__init__(hidden_size, num_branches, name=name)
-
         self.transformer = transformer.make_transformer_encoder(
             output_size=2,
             embedding_dim=hidden_size,
@@ -111,6 +99,4 @@ class ChorusTransformer(Chorus):
         hx = super().process(x, h0)
         hx = jnp.reshape(hx, (-1, self.num_branches, self.hidden_size))
         hx = self.transformer(hx)
-        hx = jnp.mean(hx, axis=1)
-
-        return hx
+        return jnp.mean(hx, axis=1)
