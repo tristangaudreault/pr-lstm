9ab4772276e7a552e15e907b749e8182acfb6a1a
Tristan Gaudreault <tristan@tristangaudreault.com>
2025-05-20 15:10:40 -0400

    Removed testing for now

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,23 +2,3 @@ import numpy as np
 import jax.numpy as jnp
 
 from thesis import models
-
-
-def test_adapt_input():
-    x = jnp.array(
-        [
-            [[1.0, 0.0], [0.0, 2.0], [3.0, 0.0], [0.0, 4.0], [5.0, 0.0]],
-            [[0.0, 6.0], [7.0, 0.0], [0.0, 8.0], [9.0, 0.0], [0.0, 10.0]],
-        ]
-    )
-
-    adapted_x = model.Chorus.adapt_input(x, 3)
-
-    expected_adapted_x = jnp.array(
-        [
-            [[1.0, 0.0], [3.0, 0.0], [5.0, 0.0], [0.0, 6.0], [0.0, 8.0], [0.0, 10.0]],
-            [[0.0, 2.0], [0.0, 4.0], [0.0, 0.0], [7.0, 0.0], [9.0, 0.0], [0.0, 0.0]],
-        ]
-    )
-
-    np.testing.assert_array_equal(adapted_x, expected_adapted_x)
