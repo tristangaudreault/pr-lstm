dede4ddc52a6b9f263ef7c8997fc14d923094a6f
Tristan Gaudreault <tristan@tristangaudreault.com>
2026-02-08 08:40:50 -0500

    Improved code organization

--- src/thesis/models.py
+++ src/thesis/models.py
@@ -15,21 +15,6 @@ from .utils import concat_tree, get_split_array, print_grad, recursive_vmap
 logger = logging.getLogger(__name__)
 
 
-def normalize(arr, a, b, axis: int):
-    # Calculate min/max along the last axis (axis=-1)
-    # keepdims=True ensures the result is (shape..., 1) instead of (shape...)
-    arr_min = jnp.min(arr, axis=axis, keepdims=True)
-    arr_max = jnp.max(arr, axis=axis, keepdims=True)
-
-    # Standard formula, now applied per-vector
-    denom = arr_max - arr_min
-
-    # Optional: Handle cases where all elements in a row are the same
-    # denom[denom == 0] = 1.0
-
-    return a + (arr - arr_min) * (b - a) / denom
-
-
 class Speculative(hk.Module):
     """Speculative model.
 
@@ -66,29 +51,17 @@ class Speculative(hk.Module):
         """
         batch_size, seq_len, embed_dim = xs.shape
         split_idx = self.get_split_idx(seq_len)
-        if hk.running_init():
-            logger.debug("%d / %d (unrolled / scanned)", split_idx, seq_len - split_idx)
 
         h0, F = self.get_core(batch_size)
 
-        _, unrolled = self.unroll(F, xs[:, :split_idx, :], h0)
+        _, hs = hk.dynamic_unroll(
+            F, xs[:, :split_idx, :], h0, time_major=False, return_all_states=True
+        )
+
         if seq_len > split_idx:
-            hs = self.speculative_scan(F, xs[:, split_idx:, :], unrolled)
-        else:
-            hs = unrolled
+            hs = self.speculative_scan(F, xs[:, split_idx:, :], hs)
 
         return self.out_filter(hs)
-
-    def get_split_idx(self, seq_len: int):
-        flag_map = {
-            "UNROLL_FIRST": 1,
-            "UNROLL_ALL": seq_len,
-            "SCAN_LAST": seq_len - 1,
-        }
-        for flag, value in flag_map.items():
-            if int(os.environ.get(flag, 0)):
-                return min(seq_len, max(1, value))
-        return math.ceil(math.log2(seq_len))
 
     def get_core(self, batch_size: int) -> tuple[Any, hk.RNNCore]:
         F = hk.LSTM(self.hidden_size)
@@ -99,10 +72,9 @@ class Speculative(hk.Module):
 
         return h0, F
 
-    def unroll(self, F, xs, h0):
-        return hk.dynamic_unroll(F, xs, h0, time_major=False, return_all_states=True)
-
-    def get_quantizer(self) -> tuple[Any, Callable[[Array], Array]]:
+    def make_scannable(
+        self, F: Callable, xs: Array
+    ) -> tuple[Callable[[Array, Array], Array], Array]:
         hk_Q = hk.nets.VectorQuantizer(self.total_size, self.K, 0.25)
 
         S = hk_Q.embeddings.T
@@ -110,10 +82,23 @@ class Speculative(hk.Module):
 
         Q = lambda v: hk_Q(v, is_training=True)
 
-        return S, Q
+        S = jax.lax.stop_gradient(S)
+
+        _, steps = recursive_vmap(F, ((None, 0), (0, None), (0, None)))(xs, S)
+        steps = concat_tree(steps)
+
+        # Q injected in composition function
+        def compose(a, b):
+            q = Q(a)
+            indices = q["encoding_indices"][..., None]
+            composed = jnp.take_along_axis(b, indices, axis=-2)
+
+            return composed + a - jax.lax.stop_gradient(a)
+
+        return compose, steps
 
     def speculative_scan(self, F: hk.RNNCore, xs: Array, unrolled: Array) -> Array:
-        '''Associatively scans the rnn step function F by speculating on previous values
+        """Associatively scans the rnn step function F by speculating on previous values
 
         Parameters
         ----------
@@ -128,32 +113,38 @@ class Speculative(hk.Module):
         -------
         array_like
             [B, T_1 + T_2, H]
-        '''
-        S, Q = self.get_quantizer()
+        """
+        unrolled = concat_tree(unrolled)
 
-        S = jax.lax.stop_gradient(S)
-        _, steps = recursive_vmap(F, ((None, 0), (0, None), (0, None)))(xs, S)
-
-        # Q injected in composition function
-        def compose(a, b):
-            q = Q(a)
-            indices = q["encoding_indices"][..., None]
-            composed = jnp.take_along_axis(b, indices, axis=-2)
-
-            return composed + a - jax.lax.stop_gradient(a)
-
-        steps = concat_tree(steps)
+        fn, elems = self.make_scannable(F, xs)
         scanned = jax.lax.associative_scan(
-            compose,
-            steps,
+            fn,
+            elems,
             axis=1,
         )
 
         # Applying trailing Q
-        compose = jax.vmap(compose, in_axes=(None, 1), out_axes=1)
-        unrolled = concat_tree(unrolled)
+        compose = jax.vmap(fn, in_axes=(None, 1), out_axes=1)
         hs = compose(unrolled[:, -1:, :], scanned)[..., 0, :]
         hs = jnp.concatenate((unrolled, hs), axis=1)
+
         hs = self.split_array(hs)
 
         return hs
+
+    def get_split_idx(self, seq_len: int) -> int:
+        if int(os.environ.get("UNROLL_FIRST", 0)):
+            split_idx = 1
+        elif int(os.environ.get("UNROLL_ALL", 0)):
+            split_idx = seq_len
+        elif int(os.environ.get("SCAN_LAST", 0)):
+            split_idx = seq_len - 1
+        else:
+            split_idx = math.ceil(math.log2(seq_len))
+
+        split_idx = min(seq_len, max(1, split_idx))
+
+        if hk.running_init():
+            logger.debug("%d / %d (unrolled / scanned)", split_idx, seq_len - split_idx)
+
+        return split_idx
