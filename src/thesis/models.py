83a163bd7b093343d9169795afee94651ae15272
Tristan Gaudreault <tristan@tristangaudreault.com>
2026-02-10 18:14:18 -0500

    Residual implementation

--- src/thesis/models.py
+++ src/thesis/models.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable, cast
 import math
 import os
+from functools import partial
 
 import jax
 from jax import Array
@@ -57,13 +58,15 @@ class Speculative(hk.Module):
 
         max_unroll = self.get_max_unroll(seq_len)
 
+        # Residual structure not yet implemented for unrolls larger than 1
         ys, handoff_state = hk.dynamic_unroll(
             F, xs[:, :max_unroll, :], h_0, time_major=False
         )
         ys = cast(Array, ys)
+        handoff_elem = (ys[:, -1, :], handoff_state)
 
         if max_unroll < seq_len:
-            ys_ext = self.speculative_scan(F, xs[:, max_unroll:, :], handoff_state)
+            ys_ext = self.speculative_scan(F, xs[:, max_unroll:, :], handoff_elem)
             ys = jnp.concatenate((ys, ys_ext), axis=1)
 
         return ys
@@ -71,7 +74,9 @@ class Speculative(hk.Module):
     def get_core(self) -> hk.RNNCore:
         return hk.LSTM(self.hidden_size)
 
-    def speculative_scan(self, F: hk.RNNCore, xs: Array, init: Any) -> Array:
+    def speculative_scan(
+        self, F: hk.RNNCore, xs: Array, init: tuple[Array, Any]
+    ) -> Array:
         """Associatively scans the rnn step function F by speculating on previous values
 
         Parameters
@@ -95,37 +100,39 @@ class Speculative(hk.Module):
             axis=1,
         )
 
-        fn_init = jax.vmap(fn, in_axes=(None, 1), out_axes=1)
+        fn_init = jax.vmap(partial(fn, residual=False), in_axes=(None, 1), out_axes=1)
         ys, _ = fn_init(init_elem, out_elems)
         ys = ys[..., 0, :]
 
         return ys
 
     def build_scan(
-        self, F: Callable, xs: Array, init: Any
+        self, F: Callable, xs: Array, init_elem: Any
     ) -> tuple[Callable, tuple[Array, Array], tuple[Array, Array]]:
-        def compose(a, b):
-            _, a_q = a
-            b_y, b_q = b
-
-            return (a_q @ b_y, a_q @ b_q)
-
         S = hk.get_parameter(
             "S",
             (self.K, self.state_size),
-            init=hk.initializers.RandomUniform(-1, 1),
+            init=hk.initializers.RandomNormal(),
         )
+        # S = jax.lax.stop_gradient(S)
+
+        def fn(a, b, residual=True):
+            a_y, a_h = a
+            b_y, b_h = b
+
+            w = utils.soft_quantize(a_h, S, temperature=1e-1)
+            return (a_y + w @ b_y, a_h + w @ b_h)
 
         k_ys, k_hs = utils.recursive_vmap(F, ((None, 0), (0, None), (0, None)))(
             xs, self.split_state(S)
         )
         k_hs = utils.concat_tree(k_hs)
-        kq_hs = utils.soft_quantize(k_hs, S)
 
-        init_y = utils.concat_tree(init)[:, None, :]
-        init_q = utils.soft_quantize(init_y, S)
+        init_y, init_h = init_elem
+        init_y = init_y[:, None, :]
+        init_h = utils.concat_tree(init_h)[:, None, :]
 
-        return compose, (k_ys, kq_hs), (init_y, init_q)
+        return fn, (k_ys, k_hs), (init_y, init_h)
 
     def get_max_unroll(self, seq_len: int) -> int:
         if (N := os.environ.get("MAX_UNROLL")) is not None:
