307a9e407831507c3f6a525b122e8a0866807623
Tristan Gaudreault <tristan@tristangaudreault.com>
2026-02-15 17:12:39 -0500

    Cleaned implementation

--- src/thesis/models.py
+++ src/thesis/models.py
@@ -1,10 +1,6 @@
-from typing import Any, Callable, cast
 import math
-import os
-from functools import partial
 
 import jax
-from jax import Array
 import jax.numpy as jnp
 import jax.nn as jnn
 import haiku as hk
@@ -14,208 +10,39 @@ from . import utils
 
 logger = logging.getLogger(__name__)
 
-# jax.config.update("jax_log_compiles", True)
-
-
-def scan_step(fn, xs, offset=1):
-    super_slice = lambda tree, slice_obj: jax.tree.map(
-        lambda leaf: leaf[:, slice_obj, :], tree
-    )
-
-    if offset >= xs[0].shape[1]:
-        return xs
-
-    a = super_slice(xs, slice(None, -offset))
-    b = super_slice(xs, slice(offset, None))
-
-    carries = super_slice(xs, slice(None, offset))
-    updates = fn(a, b)
-
-    y = jax.tree.map(
-        lambda carry, update: jnp.concatenate((carry, update), axis=1), carries, updates
-    )
-
-    # jax.tree.map(lambda leaf: print(leaf.shape), y)
-
-    return scan_step(fn, y, offset * 2)
-
 
 class CrossTemporal(hk.Module):
     def __init__(self, hidden_size: int, name: str | None = None):
         super().__init__(name=name)
         self.hidden_size = hidden_size
 
-    def __call__(self, xs: jax.Array):
-        seq_len = xs.shape[-1]
-        embdeddings = hk.Linear(self.hidden_size)(xs)
+    def __call__(self, xs: jax.Array) -> jax.Array:
+        seq_len = xs.shape[1]
+        speculations = jax.vmap(hk.Linear(self.hidden_size), in_axes=1, out_axes=1)(xs)
 
-        ys, hs = self.single_pass(embdeddings)
-        for _ in range(math.floor(math.log2(math.log2(seq_len)))):
+        num_refinements = 1  # max(1, math.floor(math.log2(math.log2(seq_len))))
+        for _ in range(num_refinements):
+            speculations = self.refine(speculations)
             # new_ys, _ = self.single_pass(ys[:, seq_len // 2 :, :])
             # ys = jnp.concatenate((ys[:, : seq_len // 2, :], new_ys), axis=1)
-            ys, _ = self.single_pass(ys)
 
-        return ys
+        return speculations
 
-    def single_pass(self, xs):
-        batch_size, seq_len, embed_dim = xs.shape
+    def refine(self, xs: jax.Array) -> jax.Array:
+        batch_size = xs.shape[0]
 
-        R = hk.LSTM(self.hidden_size)
-        j_0 = R.initial_state(batch_size)
-        y_1, j_1 = jax.vmap(R, in_axes=(1, None), out_axes=1)(xs, j_0)
+        refiner = hk.LSTM(self.hidden_size)
 
-        R = jax.vmap(R)
+        s_0 = refiner.initial_state(batch_size)
+        y_1, s_1 = jax.vmap(refiner, in_axes=(1, None), out_axes=1)(xs, s_0)
 
-        def make_fn(op):
-            def fn(a, b):
-                (inputs, (_, _)), (_, prev_state) = a, b
-
-                return op(inputs, prev_state)
-
-            return fn
-
-        return jax.lax.associative_scan(make_fn(R), (y_1, j_1), axis=1)
-
-
-class Speculative(hk.Module):
-    """Speculative model.
-
-    Attributes
-    ----------
-    K : int
-        Number of speculations points.
-    hidden_size : int
-        Hidden size of the inner RNN cells.
-    """
-
-    def __init__(
-        self,
-        K: int,
-        hidden_size: int,
-        name: str | None = None,
-    ):
-        super().__init__(name=name)
-        self.K = K
-        self.hidden_size = hidden_size
-
-    def __call__(self, xs: jax.Array) -> jax.Array:
-        """Applies the speculative model.
-
-        Parameters
-        ----------
-        xs : array_like
-            Input sequence tensor of shape ``(B, T, I)``.
-
-        Returns
-        -------
-        array_like
-            Output sequence tensor of shape (B, T, H).
-        """
-        batch_size, seq_len, embed_dim = xs.shape
-
-        F = self.get_core()
-
-        h_0 = F.initial_state(batch_size)
-        self.split_state, self.state_size = utils.get_split_array(h_0)
-
-        max_unroll = self.get_max_unroll(seq_len)
-
-        # Residual structure not yet implemented for unrolls larger than 1
-        ys, handoff_state = hk.dynamic_unroll(
-            F, xs[:, :max_unroll, :], h_0, time_major=False
-        )
-        ys = cast(Array, ys)
-        handoff_elem = (ys[:, -1, :], handoff_state)
-
-        if max_unroll < seq_len:
-            ys_ext = self.speculative_scan(F, xs[:, max_unroll:, :], handoff_elem)
-            ys = jnp.concatenate((ys, ys_ext), axis=1)
-
-        return ys
-
-    def get_core(self) -> hk.RNNCore:
-        return hk.LSTM(self.hidden_size)
-
-    def speculative_scan(
-        self, F: hk.RNNCore, xs: Array, init: tuple[Array, Any]
-    ) -> Array:
-        """Associatively scans the rnn step function F by speculating on previous values
-
-        Parameters
-        ----------
-        F : hk.RNNCore
-            RNN step function
-        xs : array_like
-            Input array of shape (B, T, E)
-        init_state : array_like
-            Initial state array of shape (B, H). Collapses speculations
-
-        Returns
-        -------
-        array_like
-            Scanned outputs of shape (B, T, H)
-        """
-        fn, elems, init_elem = self.build_scan(F, xs, init)
-        out_elems = jax.lax.associative_scan(
-            fn,
-            elems,
-            axis=1,
-        )
-
-        fn_init = jax.vmap(fn, in_axes=(None, 1), out_axes=1)
-        ys, _ = fn_init(init_elem, out_elems)
-        ys = ys[..., 0, :]
-
-        # print(ys[0])
-
-        return ys
-
-    def build_scan(
-        self, F: Callable, xs: Array, init_elem: Any
-    ) -> tuple[Callable, tuple[Array, Array], tuple[Array, Array]]:
-        S = hk.get_parameter(
-            "S",
-            (self.K, self.state_size),
-            init=hk.initializers.RandomUniform(minval=-1, maxval=1),
-        )
+        refiner = jax.vmap(refiner, in_axes=1, out_axes=1)
 
         def fn(a, b):
-            a_y, a_h = a
-            b_y, b_h = b
+            (inputs, _), (_, prev_state) = a, b
 
-            # a_h = hypermap(a_h)
+            return refiner(inputs, prev_state)
 
-            w = utils.soft_quantize(a_h, S)
-            # d = utils.soft_quantize_dists(a_h, S)
-            # w = jnn.softmax(
-            #     -(d + hypermap(d)) / 1e-1
-            # )
-            # utils.soft_quantize((S + a_h) if residual else a_h, S, temperature=1e-1)
-            return (w @ b_y, w @ b_h)
+        ys, hs = jax.lax.associative_scan(fn, (y_1, s_1), axis=1)
 
-        k_ys, k_hs = utils.recursive_vmap(F, ((None, 0), (0, None), (0, None)))(
-            xs, self.split_state(S)
-        )
-        k_hs = utils.concat_tree(k_hs)
-
-        init_y, init_h = init_elem
-        init_h = utils.concat_tree(init_h)
-        init_y, init_h = init_y[:, None, :], init_h[:, None, :]
-        return fn, (k_ys, k_hs), (init_y, init_h)
-
-    def get_max_unroll(self, seq_len: int) -> int:
-        if (N := os.environ.get("MAX_UNROLL")) is not None:
-            max_unroll = int(N)
-        elif (N := os.environ.get("MAX_SCAN")) is not None:
-            max_unroll = seq_len - int(N)
-        else:
-            max_unroll = math.ceil(math.log2(seq_len))
-
-        max_unroll = min(seq_len, max(1, max_unroll))
-
-        if hk.running_init():
-            logger.debug(
-                "%d / %d (unrolled / scanned)", max_unroll, seq_len - max_unroll
-            )
-
-        return max_unroll
+        return ys
