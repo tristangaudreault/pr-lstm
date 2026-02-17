66069ec12b4ec1eac7334abeed6662dc40111604
Tristan Gaudreault <tristan@tristangaudreault.com>
2026-02-17 13:27:20 -0500

    Improved implementation

--- src/thesis/models.py
+++ src/thesis/models.py
@@ -1,4 +1,5 @@
 import math
+from typing import Any, Callable
 
 import jax
 import jax.numpy as jnp
@@ -9,40 +10,59 @@ import logging
 from . import utils
 
 logger = logging.getLogger(__name__)
+z = tuple[jax.Array, Any]
 
 
 class CrossTemporal(hk.Module):
-    def __init__(self, hidden_size: int, name: str | None = None):
+    def __init__(
+        self,
+        hidden_size: int,
+        connection: Callable[[z, z], z] = lambda a, b: (a[0], b[1]),
+        iterations: Callable[[int], int] = lambda seq_len: 1,
+        time_axis: int = 1,
+        name: str | None = None,
+    ):
         super().__init__(name=name)
         self.hidden_size = hidden_size
+        self.connection = connection
+        self.iterations = iterations
+        self.time_axis = time_axis
+
+    def vmap_time(self, fn):
+        return jax.vmap(fn, in_axes=self.time_axis, out_axes=self.time_axis)
 
     def __call__(self, xs: jax.Array) -> jax.Array:
-        seq_len = xs.shape[1]
-        speculations = jax.vmap(hk.Linear(self.hidden_size), in_axes=1, out_axes=1)(xs)
+        speculations = self.speculate(xs)
 
-        num_refinements = 1  # max(1, math.floor(math.log2(math.log2(seq_len))))
-        for _ in range(num_refinements):
-            speculations = self.refine(speculations)
-            # new_ys, _ = self.single_pass(ys[:, seq_len // 2 :, :])
-            # ys = jnp.concatenate((ys[:, : seq_len // 2, :], new_ys), axis=1)
+        iterations = self.iterations(xs.shape[self.time_axis])
+        for n in range(iterations):
+            speculations = self.iterate(speculations)
 
-        return speculations
-
-    def refine(self, xs: jax.Array) -> jax.Array:
-        batch_size = xs.shape[0]
-
-        refiner = hk.LSTM(self.hidden_size)
-
-        s_0 = refiner.initial_state(batch_size)
-        y_1, s_1 = jax.vmap(refiner, in_axes=(1, None), out_axes=1)(xs, s_0)
-
-        refiner = jax.vmap(refiner, in_axes=1, out_axes=1)
-
-        def fn(a, b):
-            (inputs, _), (_, prev_state) = a, b
-
-            return refiner(inputs, prev_state)
-
-        ys, hs = jax.lax.associative_scan(fn, (y_1, s_1), axis=1)
+        ys, hs = speculations
 
         return ys
+
+    def speculate(self, xs: jax.Array) -> z:
+        """
+        Args:
+            xs (array_like): Input array.
+        """
+        batch_size = xs.shape[0]
+
+        speculator = hk.LSTM(self.hidden_size, name="F_theta")
+        initial_state = speculator.initial_state(batch_size)
+        speculator = jax.vmap(
+            speculator, in_axes=(self.time_axis, None), out_axes=self.time_axis
+        )
+
+        return speculator(xs, initial_state)
+
+    def iterate(self, speculations: z) -> z:
+        operator = hk.LSTM(self.hidden_size, name="F_phi")
+        operator = self.vmap_time(operator)
+
+        return jax.lax.associative_scan(
+            lambda a, b: operator(*self.connection(a, b)),
+            speculations,
+            axis=self.time_axis,
+        )
