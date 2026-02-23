29d19c7430aefc6497da5ae0549ca52732596920
Tristan Gaudreault <tristan@tristangaudreault.com>
2026-02-23 09:41:58 -0500

    Made the initial mapping G more generic for pytrees

--- src/thesis/model.py
+++ src/thesis/model.py
@@ -4,6 +4,7 @@ import jax
 import jax.numpy as jnp
 import haiku as hk
 
+from . import utils
 
 logger = logging.getLogger(__name__)
 
@@ -21,28 +22,23 @@ class CrossTemporal(hk.Module):
 
     def __call__(self, xs: jax.Array) -> jax.Array:
         cell = hk.LSTM(self.hidden_size)
-        cell_vmap_inputs = jax.vmap(
-            cell, in_axes=(self.time_axis, None), out_axes=self.time_axis
-        )
-        cell_vmap = jax.vmap(cell, in_axes=self.time_axis, out_axes=self.time_axis)
 
         initial_state = cell.initial_state(xs.shape[0])
-        initial_inputs = hk.Linear(
-            self.hidden_size * (2 if isinstance(cell, hk.LSTM) else 1)
-        )(xs)
-        _, speculations = cell_vmap_inputs(initial_inputs, initial_state)
+        inputs = utils.map_with_index(
+            lambda i, leaf: hk.Linear(leaf.shape[-1], name="FC" + str(i))(xs),
+            initial_state,
+        )
+
+        cell = jax.vmap(cell, in_axes=self.time_axis, out_axes=self.time_axis)
 
         def operator(a, b):
-            _, h = cell_vmap(jnp.concatenate(jax.tree_util.tree_leaves(a), axis=-1), b)
+            _, h = cell(jnp.concatenate(jax.tree_util.tree_leaves(a), axis=-1), b)
             return h
 
         ys = jax.lax.associative_scan(
             operator,
-            speculations,
+            inputs,
             axis=self.time_axis,
         )
 
-        if isinstance(cell, hk.LSTM):
-            ys, _ = ys
-
-        return ys
+        return jax.tree.leaves(ys)[0]
